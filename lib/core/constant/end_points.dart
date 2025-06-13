c2bfba94fa68bc6d2eaf8efecc51619f4f6e49cc
EbramIbrahim <abramibrahem22@gmail.com>
2025-06-13 21:07:15 +0300

    apply router for navigation

--- lib/core/constant/end_points.dart
+++ lib/core/constant/end_points.dart
@@ -1,15 +1,6 @@
 class EndPoints {
-  static const baseURL = 'https://netflix-backend-production-8413.up.railway.app/';
-  static const apiV = 'api/v1/';
+  static const baseURL = 'https://api.themoviedb.org/3/';
+  static const  apiKey = '7b8b48868f94b427e862e29fedb3f3ef';
+  static const  imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
 
-  static const signup = '${apiV}auth/signup';
-  static const signIn = '${apiV}auth/signin';
-  static const trendingMovies = '${apiV}movie/trending';
-  static const nowPlayingMovies = '${apiV}movie/nowplaying';
-  static const popularTV = '${apiV}tv/popular';
-  static const movie = '${apiV}movie/';
-  static const tv = '${apiV}tv/';
-  static const search = '${apiV}search/';
-
-  static const trailerBase = 'https://www.youtube.com/watch?v=';
 }
