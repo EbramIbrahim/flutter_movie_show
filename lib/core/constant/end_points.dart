6057315c6b2fffc9bcad67bd8e9f220f0e64b074
EbramIbrahim <abramibrahem22@gmail.com>
2025-06-14 17:19:22 +0300

    applying .env file to secure api key

--- lib/core/constant/end_points.dart
+++ lib/core/constant/end_points.dart
@@ -1,6 +1,5 @@
 class EndPoints {
   static const baseURL = 'https://api.themoviedb.org/3/';
-  static const  apiKey = '7b8b48868f94b427e862e29fedb3f3ef';
   static const  imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_show/core/theme/app_theme.dart';
-import 'package:movie_show/features/splash/presentation/splash_cubit.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movie_show/service_locator.dart';
 
 import 'core/navigation/router_generation.dart';
-import 'features/splash/presentation/splash_screen.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   setUpServiceLocator();
   runApp(const MyApp());
